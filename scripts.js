981a0213444cea1bd89cc1669dee9b3e8a24a971
Sébastien Vermeille <sebastien.vermeille@gmail.com>
2017-05-15 23:13:05 +0200

    a

--- scripts.js
+++ scripts.js
@@ -6,21 +6,21 @@ function locationHashChanged() {
         filter = splitted[1];
 
         if(filter){
-
-            $('#howtos-container > li').each(function(){
-                var tags = JSON.parse($(this).attr('data-tags'));
-                console.log(tags);
-                if($.inArray(filter, tags)> -1){
-                    // is concerned
-                    if($(this).is(":hidden")){
-                        $(this).show();
-                    }
-                } else{
-                    if($(this).is(":visible")){
-                        $(this).fadeOut( "slow" );
-                    }
-                }
-            });
+            //
+            // $('#howtos-container > li').each(function(){
+            //     var tags = JSON.parse($(this).attr('data-tags'));
+            //     console.log(tags);
+            //     if($.inArray(filter, tags)> -1){
+            //         // is concerned
+            //         if($(this).is(":hidden")){
+            //             $(this).show();
+            //         }
+            //     } else{
+            //         if($(this).is(":visible")){
+            //             $(this).fadeOut( "slow" );
+            //         }
+            //     }
+            // });
         }
 
     }
