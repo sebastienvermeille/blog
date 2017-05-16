0e6f98664a0ab222403591b07399d1ce7ff38546
Sébastien Vermeille <sebastien.vermeille@gmail.com>
2017-05-16 16:37:00 +0200

    a

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -29,13 +29,24 @@ function locationHashChanged() {
 window.addEventListener("hashchange", locationHashChanged, false);
 
 // HOWTO Page
-
-
 $(function() {
 
     if ( $( "#howtos-container" ).length ) {
         var mixer = mixitup('#howtos-container');
     }
+
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 
     $('h2').each(function(){
         var title = $(this).text();
