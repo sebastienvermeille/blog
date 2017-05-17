7fca9ab4d61d3e5bf05cbfb223c404b2df9343ff
Sébastien Vermeille <sebastien.vermeille@gmail.com>
2017-05-17 22:44:09 +0200

    a

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -96,15 +96,17 @@ if( window.canRunAds === undefined ){
 // Quotes
 //----------------------------------------------------------
 // Returns a random number betweein min (incl) and max (excl)
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
 }
 
 $(document).ready(function(){
 
     $.getJSON( "../../quotes.json", function( data ) {
         console.log(data);
-        var index = getRandomArbitrary(0, data.length);
+        var index = getRandomIntInclusive(0, data.length-1);
         console.log(index);
         var quote = data[index];
         console.log(quote);
